75952c21011d1605eaefc5b714ec528b811c04bc
Juan Carvajal <pcjvan@gmail.com>
2020-01-26 20:46:34 -0600

    add video showcase section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
-const loadClass = () => {
-  const heroBoxElem = document.querySelector('.showcase-animation');
-  heroBoxElem.classList.add('primary-transition');
-};
+// const loadClass = () => {
+//   const heroBoxElem = document.querySelector('.showcase-animation');
+//   heroBoxElem.classList.add('primary-transition');
+// };
 
-window.onload = loadClass;
+// window.onload = loadClass;
+
+/**
+ * Next lines will try the same as vanilla but with jQuery
+ */
+
+$(document).ready(function() {
+  $('div.showcase-animation').addClass('primary-transition');
+  // $(window).scroll(function(e) {
+  //   console.log(this.scrollY);
+  // });
+  $('#video-showcase .play-button button').on('click', function() {
+    $(this).hide();
+    $('#video-showcase .video-player').addClass('active');
+    $('body').addClass('video-isActive');
+  });
+});
+// window.addEventListener('scroll', function(e) {
+//   // console.log(e);
+//   console.log(this.scrollY);
+// });

1d168f4bc803b3bd66368a7f6d5cf73b5ad0f1ca
Juan Carvajal <pcjvan@gmail.com>
2020-02-14 23:06:33 -0600

    move variable template to its own file

---
+++ js/video_player_template.js
@@ -0,0 +1,16 @@
+const videoPlayerTemplate = `
+  <div class="video-player-wrapper">
+    <div id="overlay"></div>
+    <div class="video-player toPlay">
+      <div class="control">
+        <iframe
+          src="https://player.vimeo.com/video/46065546"
+          width="800"
+          height="500"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
+        <div class="esc-video">X</div>
+      </div>
+    </div>
+  </div`;
